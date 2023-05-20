ce8d3c0b54474f26602a354298b3234adb8b81bf
Miguel Cabrera <macabrerad06@gmail.com>
2023-05-20 18:52:24 -0500

    Practicando con listas objetos

---
+++ Java/Persona.java
@@ -0,0 +1,27 @@
+
+
+public class Persona {
+    //atributos
+    private String nombre;
+    private int edad;
+
+
+
+    //metodos
+    public int getEdad() {
+        return edad;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    
+}
